6cc794bdca5bf033adbaa02adf14db6d67c47b4c
denev88 <119948803+denev88@users.noreply.github.com>
2023-10-09 18:35:57 +0300

    Create 02. Day of Week.js

---
+++ Array/02. Day of Week.js
@@ -0,0 +1,13 @@
+function dayOfWeek (day){
+    let days = [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" ];
+    if (day >= 1 && day <= 7) {
+        console.log(days[day - 1])
+        
+    } else {
+        console.log ("Invalid day!")
+        
+    }
+}
+dayOfWeek(1)
+dayOfWeek(8)
+dayOfWeek(0)
